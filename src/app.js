b08e5e540b3a43459f6af23e4c3330a0713e23d1
Sebastián Velásquez <salexandervelasquez@ucundinamarca.edu.co>
2025-05-28 20:56:30 -0500

    Despliegue en Railway

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import eventRoutes from "./routes/events.routes.js";
 
 const app = express();
 
+app.use(cors());  //CORS para todas las rutas
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
